275397840d0bc637a419b21291240f1e72b09052
Taras Romaniuk <tarasrom25@gmail.com>
2024-05-06 23:07:27 +0300

    check if user with email already exists

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api');
 
   const PORT = app.get(ConfigService).get<number>('PORT');
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersRepository } from './users.repository';
@@ -8,6 +8,15 @@ export class UsersService {
   constructor(private readonly userRepository: UsersRepository) {}
 
   create(createUserDto: CreateUserDto) {
+    const existingUserEmail = this.userRepository.findByEmail(
+      createUserDto.email,
+    );
+    if (existingUserEmail) {
+      throw new BadRequestException(
+        `User with email: ${createUserDto.email} already exists`,
+      );
+    }
+
     return this.userRepository.create(createUserDto);
   }
 
@@ -20,6 +29,15 @@ export class UsersService {
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
+    const existingUserEmail = this.userRepository.findByEmail(
+      updateUserDto.email,
+    );
+    if (existingUserEmail) {
+      throw new BadRequestException(
+        `User with email: ${updateUserDto.email} already exists`,
+      );
+    }
+
     return this.userRepository.update(id, updateUserDto);
   }
 
