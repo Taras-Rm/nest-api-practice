326a495dcb312e11621ecde208a1b09230de9759
Taras Romaniuk <tarasrom25@gmail.com>
2024-05-06 23:26:44 +0300

    added validations

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,12 +1,24 @@
-import { IsEmail, IsInt, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsInt,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @MinLength(3)
+  @MaxLength(100)
   name: string;
 
   @IsEmail()
   email: string;
 
   @IsInt()
+  @Min(1)
+  @Max(200)
   age: number;
 }
--- server/src/users/dto/update-user.dto.ts
+++ server/src/users/dto/update-user.dto.ts
@@ -1,12 +1,23 @@
-import { IsEmail, IsInt, IsString } from 'class-validator';
-
+import {
+  IsEmail,
+  IsInt,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
 export class UpdateUserDto {
   @IsString()
+  @MinLength(3)
+  @MaxLength(100)
   name: string;
 
   @IsEmail()
   email: string;
 
   @IsInt()
+  @Min(1)
+  @Max(200)
   age: number;
 }
